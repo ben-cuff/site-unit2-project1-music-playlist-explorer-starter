c934236f751809987cef0654c6577c5358419619
Ben Cuff <bencuff14@gmail.com>
2025-06-04 12:03:24 -0700

    added like button images and modal display depending onthe one pressed

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -26,7 +26,7 @@ function renderPlaylists() {
 		playlistDivElement.id = playlist.playlistID;
 
 		const playlistImage = document.createElement("img");
-		playlistImage.src = playlist.playlist_art;
+		playlistImage.src = playlist.playlist_art || "assets/img/playlist.png";
 		playlistDivElement.appendChild(playlistImage);
 
 		const playlistTitle = document.createElement("h2");
@@ -44,9 +44,20 @@ function renderPlaylists() {
 		likeButton.addEventListener("click", (event) => {
 			toggleLike(event);
 		});
+
+		const likeIcon = document.createElement("img");
+		likeIcon.src = likeButton.getAttribute("data-liked")
+			? "assets/img/heart.svg"
+			: "assets/img/heart-outline.svg";
+		likeButton.appendChild(likeIcon);
+
 		playlistDivElement.appendChild(likeButton);
 
 		playlistContainer.appendChild(playlistDivElement);
+
+		playlistDivElement.addEventListener("click", () => {
+			renderModal(playlist);
+		});
 	});
 }
 
@@ -63,4 +74,75 @@ function toggleLike(event) {
 			parseInt(event.target.textContent.split(": ")[1]) + 1
 		}`;
 	}
+}
+
+function renderModal(playlist) {
+	const modalContainer = document.querySelector(".modal-container");
+	modalContainer.innerHTML = "";
+	const modalDiv = document.createElement("div");
+	modalDiv.classList.add("modal");
+
+	const modalContent = document.createElement("div");
+	modalContent.classList.add("modal-content");
+
+	const modalHeader = document.createElement("div");
+	modalHeader.classList.add("modal-header");
+
+	const headerImg = document.createElement("img");
+	headerImg.src = playlist.playlist_art || "assets/img/playlist.png";
+	modalHeader.appendChild(headerImg);
+
+	const headerTextWrapper = document.createElement("div");
+	headerTextWrapper.classList.add("modal-header-text-wrapper");
+
+	const headerTitle = document.createElement("h3");
+	headerTitle.textContent = playlist.playlist_name;
+	headerTextWrapper.appendChild(headerTitle);
+
+	const headerAuthor = document.createElement("p");
+	headerAuthor.textContent = playlist.playlist_author;
+	headerTextWrapper.appendChild(headerAuthor);
+
+	modalHeader.appendChild(headerTextWrapper);
+	modalContent.appendChild(modalHeader);
+
+	const songCardWrapper = document.createElement("div");
+	songCardWrapper.classList.add("song-card-wrapper");
+
+	if (playlist.songs && Array.isArray(playlist.songs)) {
+		playlist.songs.forEach((song) => {
+			const songCard = document.createElement("div");
+			songCard.classList.add("song-card");
+
+			const songImg = document.createElement("img");
+			songImg.src = song.image || "assets/img/song.png";
+			songCard.appendChild(songImg);
+
+			const songCardText = document.createElement("div");
+			songCardText.classList.add("song-card-text");
+
+			const songTitle = document.createElement("h4");
+			songTitle.textContent = song.title;
+			songCardText.appendChild(songTitle);
+
+			const songArtist = document.createElement("p");
+			songArtist.textContent = song.artist;
+			songCardText.appendChild(songArtist);
+
+			const songAlbum = document.createElement("p");
+			songAlbum.textContent = song.album;
+			songCardText.appendChild(songAlbum);
+
+			const duration = document.createElement("p");
+			duration.textContent = song.duration;
+			songCardText.appendChild(duration);
+
+			songCard.appendChild(songCardText);
+			songCardWrapper.appendChild(songCard);
+		});
+	}
+
+	modalContent.appendChild(songCardWrapper);
+	modalDiv.appendChild(modalContent);
+	modalContainer.appendChild(modalDiv);
 }
